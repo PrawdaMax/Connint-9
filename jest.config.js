173dcf882f83cceab507adbb99512282e4055edf
prawdam <prawdamaximilian@gmail.com>
2026-06-09 17:10:17 +0200

    test

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,5 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  // This explicitly tells Jest to look anywhere inside your project directory
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
 };
