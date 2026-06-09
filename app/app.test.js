6ad388cfe8785543fb81d3e10b1cac25f35de706
prawdam <prawdamaximilian@gmail.com>
2026-06-09 16:49:50 +0200

    app

---
+++ app/app.test.js
@@ -0,0 +1,6 @@
+describe('Prisma & App Environment Test', () => {
+  it('should successfully pass a sanity check', () => {
+    const total: number = 10 + 5;
+    expect(total).toBe(15);
+  });
+});
